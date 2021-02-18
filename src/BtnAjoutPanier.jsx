c202d2003e703b050e5eb51540d6e38eb588e484
Olivier Fortier <olivier_fortier@outlook.com>
2021-02-18 16:15:33 -0500

    découplage du bouton

--- src/BtnAjoutPanier.jsx
+++ src/BtnAjoutPanier.jsx
@@ -3,12 +3,12 @@ import { useState } from "react";
 import "./BtnAjoutPanier.scss";
 
 
-export default function BtnAjoutPanier({ onClick, qte, texte }) {
+export default function BtnAjoutPanier({ onClick, qte, texte , classeCss}) {
   return (
     <Badge badgeContent={qte} color="primary">
       <button
         onClick={onClick}
-        className={`BtnAjoutPanier ${qte > 0 && "rouge"}`}
+        className={`BtnAjoutPanier ${classeCss}`}
       >
         {texte}
       </button>
--- src/Produit.jsx
+++ src/Produit.jsx
@@ -1,29 +1,34 @@
-import BtnAjoutPanier from './BtnAjoutPanier';
-import './Produit.scss';
+import BtnAjoutPanier from "./BtnAjoutPanier";
+import "./Produit.scss";
 import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
 
-export default function Produit({prix, id, etatPanier, nom}) {
-
+export default function Produit({ prix, id, etatPanier, nom }) {
   const [panier, setPanier] = etatPanier;
 
   function ajoutPanier() {
-    if(panier[id]) {
+    if (panier[id]) {
       panier[id].qte++;
+    } else {
+      panier[id] = { nom: nom, prix: prix, qte: 1 };
     }
-    else {
-     panier[id] = {nom: nom, prix: prix, qte: 1};
-    }
-    setPanier({...panier});
+    setPanier({ ...panier });
   }
 
-  return(
+  return (
     <li className="Produit">
-      <img src={'images-produits/' + id + '.webp'} alt=""/>
+      <img src={"images-produits/" + id + ".webp"} alt="" />
       <div className="info">
         <p className="nom">{nom}</p>
         <p className="prix">{prix}</p>
       </div>
-      <BtnAjoutPanier onClick={ajoutPanier} qte={panier[id]?.qte || 0} texte={panier[id]?.qte > 0 ? <AddShoppingCartIcon/> : 'Ajouter au panier'} />
+      <BtnAjoutPanier
+        onClick={ajoutPanier}
+        qte={panier[id]?.qte || 0}
+        texte={
+          panier[id]?.qte > 0 ? <AddShoppingCartIcon /> : "Ajouter au panier"
+        }
+        classeCss={panier[id]?.qte > 0 && 'rouge'}
+      />
     </li>
   );
 }
