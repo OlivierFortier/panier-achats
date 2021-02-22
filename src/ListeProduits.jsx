34ce6aa83810d1657f2123e59adf7ce559f769d4
Olivier Fortier <olivier_fortier@outlook.com>
2021-02-22 10:58:56 -0500

    configuration préliminaire de firebase

--- src/ListeProduits.jsx
+++ src/ListeProduits.jsx
@@ -1,6 +1,17 @@
 import './ListeProduits.scss';
 import Produit from "./Produit";
 import tabProduits from './data/produits.json';
+import fbAppli from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDEWV0JTpiNCamY6BciAfQzNZfKSLvklAY",
+  authDomain: "pvt-h21-olivier.firebaseapp.com",
+  projectId: "pvt-h21-olivier",
+  storageBucket: "pvt-h21-olivier.appspot.com",
+  messagingSenderId: "705844162303",
+  appId: "1:705844162303:web:f92863b5cc0e360a61a6ca"
+};
 
 export default function ListeProduits({etatPanier}) {
 
