9b66207a27f934512a5428495b249b25453925ba
Olivier Fortier <olivier_fortier@outlook.com>
2021-02-22 09:56:39 -0500

    gh pages hack

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/https://OlivierFortier.github.io/panier-achats/">
       <Appli />
     </Router>
   </React.StrictMode>,
